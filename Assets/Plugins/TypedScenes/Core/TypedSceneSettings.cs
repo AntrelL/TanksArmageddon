17efde6f8275705e20e84c28b09a33709e633176
AntrelL <Nikitagaliant03@bk.ru>
2024-06-18 03:37:47 +0300

    Build: add basic plugins
    TextMesh Pro, TypedScenes

---
+++ Assets/Plugins/TypedScenes/Core/TypedSceneSettings.cs
@@ -0,0 +1,12 @@
+﻿namespace IJunior.TypedScenes
+{
+    public static class TypedSceneSettings
+    {
+        public const string Namespace = "IJunior.TypedScenes";
+        public const string BaseClass = "TypedScene";
+        public const string SavingDirectory = "Assets/Source/Game/Scenes/Typed/";
+        public const string ClassExtension = ".cs";
+        public const string SceneExtension = ".unity";
+        public const string MetaExtension = ".meta";
+    } 
+}

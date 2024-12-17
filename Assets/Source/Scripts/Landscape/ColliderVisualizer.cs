3f6b35a0358f8fa759c24ca78a3bee18dffca3c6
AntrelL <Nikitagaliant03@bk.ru>
2024-12-18 02:53:31 +0300

    Add test environment

---
+++ Assets/Source/Scripts/Landscape/ColliderVisualizer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+namespace TanksArmageddon.Landscape
+{
+    public class ColliderVisualizer : MonoBehaviour
+    {
+        [SerializeField] private Collider2D _collider;
+        [SerializeField] private MeshFilter _meshFilter;
+
+        private void Start()
+        {
+            Mesh mesh = _collider.CreateMesh(true, true);
+            _meshFilter.mesh = mesh;
+        }
+    }
+}

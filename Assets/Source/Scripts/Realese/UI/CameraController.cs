cf622f9319a7eaa9f42bd79029f4c375e6a5336a
RyzhVrn <rabochy.ak@yandex.ru>
2025-02-06 19:11:18 +0300

    Added camera tracking

---
+++ Assets/Source/Scripts/Realese/UI/CameraController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    [SerializeField] private Transform _enemy;
+    [SerializeField] private float _moveSpeed = 3f;
+    [SerializeField] private float _followSpeed = 5f;
+    [SerializeField] private float _waitTime = 1.5f;
+
+    private bool followingPlayer = false;
+
+    private void Start()
+    {
+        StartCoroutine(CameraIntroSequence());
+    }
+
+    private IEnumerator CameraIntroSequence()
+    {
+        yield return MoveToTarget(_player.position);
+        yield return new WaitForSeconds(_waitTime);
+
+        yield return MoveToTarget(_enemy.position);
+        yield return new WaitForSeconds(_waitTime);
+
+        yield return MoveToTarget(_player.position);
+        yield return new WaitForSeconds(_waitTime);
+
+        followingPlayer = true;
+    }
+
+    private IEnumerator MoveToTarget(Vector3 targetPosition)
+    {
+        targetPosition.z = transform.position.z;
+
+        while (Vector3.Distance(transform.position, targetPosition) > 0.1f)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, _moveSpeed * Time.deltaTime);
+            yield return null;
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if (followingPlayer && _player != null)
+        {
+            Vector3 targetPosition = new Vector3(_player.position.x, _player.position.y, transform.position.z);
+            transform.position = Vector3.Lerp(transform.position, targetPosition, _followSpeed * Time.deltaTime);
+        }
+    }
+}

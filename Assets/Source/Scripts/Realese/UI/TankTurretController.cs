9e9d2fa29308a4983d685501354ba7e39b35f9f1
RyzhVrn <rabochy.ak@yandex.ru>
2025-02-06 18:15:41 +0300

    Update player turret control

--- Assets/Source/Scripts/Realese/UI/TankTurretController.cs
+++ Assets/Source/Scripts/Realese/UI/TankTurretController.cs
@@ -6,23 +6,36 @@ public class TankTurretController : MonoBehaviour
 {
     [SerializeField] private Transform _turret;
     [SerializeField] private Slider _angleSlider;
+    private float _initialGunAngle;
+
+    private void Start()
+    {
+        _angleSlider.onValueChanged.AddListener(OnSliderValueChanged);
+        _initialGunAngle = GetLocalGunAngle();
+    }
+
+    /*private void OnSliderValueChanged(float value)
+    {
+        _turret.rotation = Quaternion.Euler(0, 0, value);
+    }*/
+
+    private void OnSliderValueChanged(float value)
+    {
+        float clampedAngle = Mathf.Clamp(value, _angleSlider.minValue, _angleSlider.maxValue);
+        _turret.localRotation = Quaternion.Euler(0, 0, _initialGunAngle + clampedAngle);
+    }
+
+    private float GetLocalGunAngle()
+    {
+        return _turret.localEulerAngles.z;
+    }
 
     private void OnDisable()
     {
         _turret.DOKill();
     }
 
-    void Start()
-    {
-        _angleSlider.onValueChanged.AddListener(OnSliderValueChanged);
-    }
-
-    void OnSliderValueChanged(float value)
-    {
-        _turret.rotation = Quaternion.Euler(0, 0, value);
-    }
-
-    void OnDestroy()
+    private void OnDestroy()
     {
         if (_angleSlider != null)
         {

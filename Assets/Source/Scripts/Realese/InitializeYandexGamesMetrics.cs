4585c3d60aaf93377c3484e300eb55f432fad123
RyzhVrn <rabochy.ak@yandex.ru>
2025-04-09 03:29:25 +0300

    вернул InitializeYandexGamesMetrics

--- Assets/Source/Scripts/Realese/InitializeYandexGamesMetrics.cs
+++ Assets/Source/Scripts/Realese/InitializeYandexGamesMetrics.cs
@@ -5,12 +5,12 @@ using Agava.YandexGames;
 
 public class InitializeYandexGamesMetrics : MonoBehaviour
 {
-    /*
+    
     private void Start()
     {
 #if !UNITY_EDITOR && UNITY_WEBGL
         YandexGamesSdk.GameReady();
 #endif
     }
-    */
+    
 }
